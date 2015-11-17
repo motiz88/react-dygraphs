ed0cca6b96014d4abbe6ddc1cc8b704fdea496bd
Moti Zilberman <motiz88@gmail.com>
2015-11-17 17:54:19 +0200

    Integrate eclint

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -4,20 +4,15 @@ module.exports = function(config) {
     config.set({
         basePath: '',
 
-
         frameworks: ['mocha', 'chai'],
-
         files: [
             './node_modules/phantomjs-polyfill/bind-polyfill.js',
             'test/index.js',
         ],
-
         exclude: [],
-
         preprocessors: {
             "test/index.js": ["webpack", "sourcemap"],
         },
-
         webpack: {
             module: {
                 preLoaders: [{
@@ -38,47 +33,20 @@ module.exports = function(config) {
             },
             devtool: 'inline-source-map'
         },
-
         coverageReporter: {
             type: 'lcov',
             dir: 'coverage/'
         },
-
         webpackMiddleware: {
             noInfo: true
         },
-
         reporters: ['progress', 'coverage', 'coveralls'],
-
-
-        // web server port
         port: 9876,
-
-
-        // enable / disable colors in the output (reporters and logs)
         colors: true,
-
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
-
-
-        // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
-
-
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['Firefox', 'PhantomJS'],
-
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
         singleRun: true,
-
-        // Concurrency level
-        // how many browser should be started simultanous
         concurrency: Infinity
     })
 }
--- karma.conf.js
+++ karma.conf.js
@@ -3,20 +3,14 @@ var path = require('path');
 module.exports = function(config) {
     config.set({
         basePath: '',
-
-
         frameworks: ['mocha', 'chai'],
-
         files: [
             'test/index.js',
         ],
-
         exclude: [],
-
         preprocessors: {
             "test/index.js": ["webpack", "sourcemap"],
         },
-
         webpack: {
             module: {
                 preLoaders: [{
@@ -37,50 +31,20 @@ module.exports = function(config) {
             },
             devtool: 'inline-source-map'
         },
-
         coverageReporter: {
             type: 'html',
             dir: 'coverage/'
         },
-
         webpackMiddleware: {
             noInfo: true
         },
-
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['progress', 'coverage'],
-
-
-        // web server port
         port: 9876,
-
-
-        // enable / disable colors in the output (reporters and logs)
         colors: true,
-
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
-
-
-        // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
-
-
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['Chrome'],
-
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
         singleRun: false,
-
-        // Concurrency level
-        // how many browser should be started simultanous
         concurrency: Infinity
     })
 }
