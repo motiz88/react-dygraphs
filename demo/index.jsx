06fbda53b3ae96c26cce01d204b10a186aadc4ca
Moti Zilberman <motiz88@gmail.com>
2015-11-18 15:36:59 +0200

    Initial actual use of dygraphs

--- demo/index.jsx
+++ demo/index.jsx
@@ -4,4 +4,4 @@ import {Dygraph} from '../src';
 
 var container = document.createElement('div');
 document.body.appendChild(container);
-ReactDOM.render(<Dygraph />, container);
+ReactDOM.render(<Dygraph data={[[1,10,100], [2,20,80], [3,50,60], [4,70,80]]} />, container);
--- src/components/Dygraph.jsx
+++ src/components/Dygraph.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
+import DygraphBase from 'dygraphs-commonjs';
 
 export default class Dygraph extends React.Component {
     displayName = 'Dygraph';
+
+    static propTypes = {
+        data: React.PropTypes.oneOfType([
+            React.PropTypes.string /* CSV or URL */,
+            React.PropTypes.arrayOf(React.PropTypes.oneOfType([
+                React.PropTypes.instanceOf(Date),
+                React.PropTypes.number
+            ])),
+            React.PropTypes.func
+        ]).isRequired
+    }
 
     constructor() {
         super();
     }
 
+    componentDidMount() {
+        this._dygraph = new DygraphBase(this.refs.root, this.props.data);
+    }
+
+    componentWillUpdate(nextProps/*, nextState*/) {
+        if (this._dygraph) {
+            this._dygraph.updateOptions({
+                data: nextProps.data
+            });
+        }
+    }
+
+    componentWillUnmount() {
+        if (this._dygraph) {
+            this._dygraph.destroy();
+            this._dygraph = null;
+        }
+    }
+
     render() {
-        return <div>Dygraph</div>;
+        return <div ref="root"></div>;
     }
 }
