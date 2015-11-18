709d306d3cb2df02e652e6a20a3201f3728bfa6e
Moti Zilberman <motiz88@gmail.com>
2015-11-18 18:52:20 +0200

    Add props to component constructor

--- src/components/StarterComponent.jsx
+++ src/components/StarterComponent.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default class StarterComponent extends React.Component {
     displayName = 'StarterComponent';
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
     }
 
     render() {
