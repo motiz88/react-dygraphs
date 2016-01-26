0a9eea084edc3fe75db9249ac1c172829bc551a3
Moti Zilberman <motiz88@gmail.com>
2016-01-26 13:29:11 +0200

    Slightly beef up demo code

--- demo/index.jsx
+++ demo/index.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Dygraph} from '../src';
+import util from 'util';
 
 var container = document.createElement('div');
 document.body.appendChild(container);
-ReactDOM.render(<Dygraph data={[[1,10,100], [2,20,80], [3,50,60], [4,70,80]]} />, container);
+
+class DygraphDemo extends React.Component {
+    displayName = 'DygraphDemo';
+
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        zoom: null,
+        clicked: null,
+        data: [[1,10,100], [2,20,80], [3,50,60], [4,70,80]]
+    };
+
+    handleZoom = (min, max, yranges) => {
+        this.setState({zoom: {min, max, yranges}});
+    }
+
+    handlePointClick = (e, point) => {
+        this.setState({clicked: point.idx});
+    }
+
+    render() {
+        return (
+            <div>
+                <Dygraph
+                    data={this.state.data}
+                    onPointClick={this.handlePointClick}
+                    onZoom={this.handleZoom}
+                    strokeWidth={10}
+                />
+                {this.state.zoom ? util.inspect(this.state.zoom) : null}
+                {this.state.clicked}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<DygraphDemo />, container);
